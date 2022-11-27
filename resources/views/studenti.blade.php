36a1537f276d778c17fa6960db1f5b7ebe1f613a
prvul <petar.prvulovic@gmail.com>
2022-11-27 11:22:38 +0100

    Inicijalni commit

---
+++ resources/views/studenti.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>USI</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+  <h1>Studenti</h1>
+  <table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Ime i prezime</th>
+            <th>Broj indeksa</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Petar Prvulovic</td>
+            <td>41/07</td>
+        </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html> 
